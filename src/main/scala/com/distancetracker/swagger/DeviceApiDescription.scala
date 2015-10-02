097039ceca2137b30268da2cb58c44eabd1119e5
DClaudio <david_claudio_t@yahoo.com>
2015-10-02 11:31:22 +0100

    updated api desc

--- src/main/scala/com/distancetracker/swagger/DeviceApiDescription.scala
+++ src/main/scala/com/distancetracker/swagger/DeviceApiDescription.scala
@@ -2,7 +2,7 @@ package com.distancetracker.swagger
 
 import com.distancetracker.model.Device
 import com.distancetracker.persistence.DeviceEntity
-import org.scalatra.swagger.{Swagger, SwaggerSupport}
+import org.scalatra.swagger.{StringResponseMessage, Swagger, SwaggerSupport}
 
 /**
  * Created by claudio.david on 14/09/2015.
@@ -16,26 +16,41 @@ trait DeviceApiDescription extends SwaggerSupport {
   val createNewDeviceOperation = (apiOperation[DeviceEntity]("createNewDevice")
     summary "Create a new device in the database."
     parameter bodyParam[Device]("device").description("the device to register").required
+
+    responseMessage (StringResponseMessage(400,"invalid request"))
+    responseMessage (StringResponseMessage(201,"device created"))
     )
 
   val getDeviceByIdOperation = (apiOperation[DeviceEntity]("getDevice")
     summary "Get a device"
     parameters (pathParam[String]("deviceid").description("device id")).required
+
+    responseMessage (StringResponseMessage(400,"invalid request"))
+    responseMessage (StringResponseMessage(404,"device not found"))
+    responseMessage (StringResponseMessage(200,"device found"))
     )
 
   val deleteDeviceOperation = (apiOperation[DeviceEntity]("deleteDevice")
     summary "Delete a device"
     parameters (pathParam[String]("deviceid").description("device id")).required
+
+    responseMessage (StringResponseMessage(400,"invalid request"))
+    responseMessage (StringResponseMessage(404,"device not found"))
+    responseMessage (StringResponseMessage(204,"device succesfully deleted"))
     )
 
   val updateDeviceOperation = (apiOperation[DeviceEntity]("updateDevice")
     summary "Update a device"
     parameters (pathParam[String]("deviceid").description("device id")).required
     parameter bodyParam[Device]("device").description("the updated values").required
+
+    responseMessage (StringResponseMessage(400,"invalid request"))
+    responseMessage (StringResponseMessage(404,"device not found"))
+    responseMessage (StringResponseMessage(200,"device succesfully updated"))
     )
 
   val getDeviceListOperation = (apiOperation[Set[DeviceEntity]]("getAllDevices")
     summary "Get All devices"
     )
-  
+
 }
--- src/main/scala/com/distancetracker/swagger/GpsApiDescription.scala
+++ src/main/scala/com/distancetracker/swagger/GpsApiDescription.scala
@@ -2,7 +2,7 @@ package com.distancetracker.swagger
 
 import com.distancetracker.model.Coordinates
 import com.distancetracker.persistence.GpsDataEntity
-import org.scalatra.swagger.{Swagger, SwaggerSupport}
+import org.scalatra.swagger.{StringResponseMessage, Swagger, SwaggerSupport}
 
 
 trait GpsApiDescription extends SwaggerSupport {
@@ -14,11 +14,18 @@ trait GpsApiDescription extends SwaggerSupport {
     summary "Insert new coordinates in the database."
     parameters (pathParam[String]("deviceid").description("device id")).required
     parameter bodyParam[Coordinates]("gps coordinates").description("the coordinates to insert").required
+
+    responseMessage (StringResponseMessage(400,"invalid request"))
+    responseMessage (StringResponseMessage(201,"device created"))
     )
 
   val readDeviceCoordinates = (apiOperation[GpsDataEntity]("readDeviceCoordinates")
     summary "Read coordinates from the database."
     parameters (pathParam[String]("deviceid").description("device id")).required
+
+    responseMessage (StringResponseMessage(400,"invalid request"))
+    responseMessage (StringResponseMessage(404,"coordinates not found"))
+    responseMessage (StringResponseMessage(200,"device found"))
     )
 
 }
